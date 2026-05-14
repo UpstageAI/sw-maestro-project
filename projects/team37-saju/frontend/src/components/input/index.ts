77b6a22d3ca72b785d0fb3e94f01baf0e6cbf0d4
WithFortuna <rmsghchl0@naver.com>
2026-05-14 11:44:14 +0900

    [Team 37] 프로젝트 제출 수정 - saju

--- projects/team37-saju/frontend/src/components/input/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as DateTimeInput } from './DateTimeInput';
-export { default as OriginSelect } from './OriginSelect';
-export { default as RangeSelect } from './RangeSelect';
-export { default as DurationSelect } from './DurationSelect';
-export { default as TagPicker } from './TagPicker';
--- projects/team37-saju/frontend/src/pages/LandingPage.tsx
+++
@@ -1,7 +0,0 @@
-export default function LandingPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-mystic">
-      <p className="text-primary">LandingPage (구현 예정)</p>
-    </div>
-  );
-}
